20f64803a418f495b094a0f0bd26657091769c54
jewnikita <jewnikita@gmail.com>
2026-09-04 22:45:13 +0300

    Update generate.js

--- generate.js
+++ generate.js
@@ -60,7 +60,7 @@ const htmlContent = `<!DOCTYPE html>
   
   .stats { 
     position: absolute;
-    bottom: 1505px;           /* Было 34% */
+    top: 800px;           /* Было 34% */
     left: 50%;
     transform: translateX(-50%);
     font-size: 40px; 
